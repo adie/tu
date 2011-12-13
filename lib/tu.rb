08ecd45044bf4eeac0f8c77a8afae90523b68b9b
Anton Dieterle <antondie@gmail.com>
2011-12-13 10:44:04 +0400

    Added simple grep command

--- lib/tu.rb
+++ lib/tu.rb
@@ -2,9 +2,17 @@ require 'rubygems'
 require 'thor'
 
 class Tu < Thor
+  include Thor::Actions
+
   desc "version", "Show TU version"
   def version
     require 'tu/version'
     puts "TU #{Tu::VERSION}"
   end
+
+  desc "grep STRING_OR_REGEX", "Search for string or regexp in files within current directory filtered for rails app"
+  def grep(search)
+    run "grep -rs #{search} app config lib spec test features"
+  end
+  map "g" => "grep"
 end
