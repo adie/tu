2cedbbb8128c2dff39846f32db5d254361e83576
Anton Dieterle <antondie@gmail.com>
2011-12-22 16:49:12 +0400

    Added public/javascripts/ to grep search path

--- lib/tu.rb
+++ lib/tu.rb
@@ -12,7 +12,7 @@ class Tu < Thor
 
   desc "grep STRING_OR_REGEX", "Search for string or regexp in files within current directory filtered for rails app"
   def grep(search)
-    run "grep -rs #{search} app config lib spec test features"
+    run "grep -rs #{search} app config lib spec test features public/javascripts"
   end
   map "g" => "grep"
 end
